56627029d75f098eb05abd7768b48266d42b041b
eswaribala <parameswaribala@gmail.com>
2021-08-20 13:29:00 +0530

    updated

--- javaexercises/src/com/amex/javaexercises/JsontoMap.groovy
+++ javaexercises/src/com/amex/javaexercises/JsontoMap.groovy
@@ -1,9 +1,22 @@
 package com.amex.javaexercises
 
 import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
 
 
 //convert json string to object
 jsonObject=new JsonSlurper().parseText('{"Name":"RPS","Service":"Training"}')
 
 println "Name=$jsonObject.Name, Service=$jsonObject.Service"
+
+users=new JsonSlurper().parse(new File("users.txt"))
+
+users.each { userEntries->
+	//println userEntries
+	entries=userEntries.entrySet();
+	entries.forEach{
+		entry->
+		println entry
+	}
+	
+}
